1901cde28d11edab8b0e80977f868e326dccf3a6
Audeta <audeta35@gmail.com>
2019-09-17 21:04:15 +0700

    add prototype

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Blank Page</title>
+  <title>AdminLTE | Blank Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <link rel="shortcut icon" href="<?= base_url('assets/profile/chatt.png') ?>">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
